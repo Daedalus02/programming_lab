c3d531d9bfff4726ffd1be885afc48a59cdcff60
Daedalus_02 <88882210+fish11111@users.noreply.github.com>
2023-05-11 08:44:12 +0200

    Delete pacman directory

--- pacman/cls.java
+++
@@ -1,9 +0,0 @@
-public class cls {
-    public void clear(){
-        try{
-        new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
-        }catch(Exception e){
-            System.out.println(e);
-        }
-    }
-}
